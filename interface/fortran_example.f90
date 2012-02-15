49ae1d6ed1e9f3b788406ead943a2e42ec54c43d
astrojhgu <astrojhgu@ed2142bd-67ad-457f-ba7c-d818d4011675>
2012-02-15 15:43:22 +0000

    git-svn-id: file:///home/svn/opt_utilities@222 ed2142bd-67ad-457f-ba7c-d818d4011675

--- interface/fortran_example.f90
+++ interface/fortran_example.f90
@@ -9,9 +9,13 @@ end function foo
 program main
   double precision foo
   double precision p(2)
-  p(1)=4
-  p(2)=5
+  integer np
+  double precision precision
+  np=2
+  precision=1e-10
+  p(1)=4000
+  p(2)=5000
   write(*,*) p,foo(p)
-  call optimize_powell(foo,2,p,1E-10)
+  call optimize_powell(foo,np,p,precision)
   write(*,*) p,foo(p)
 end program main
