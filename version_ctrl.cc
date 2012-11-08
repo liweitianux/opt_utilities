a2bdb7accfcc4f26371b94207912fdfa1686019c
astrojhgu <astrojhgu@gmail.com>
2012-11-09 00:46:16 +0800

    test

--- version_ctrl.cc
+++ version_ctrl.cc
@@ -7,7 +7,7 @@ public:
 	{
 	  cerr<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
 	  cerr<<"% Flexible model fitting package      %"<<endl;
-	  cerr<<"% version !!!!                        %"<<endl;
+	  cerr<<"% version ????                        %"<<endl;
 	  cerr<<"% by Gu Junhua 2012.                  %"<<endl;
 	  cerr<<"% jhgu@bao.ac.cn                      %"<<endl;
 	  cerr<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;;
